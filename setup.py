e506dd95974222f64807745256abb617b7de66b1
Hanno Schlichting <hanno@hannosch.eu>
2010-11-02 10:01:38 +0000

    Correct license to GPL version 2 only and clean up distribution metadata
    svn path=/plone.testing/trunk/; revision=45076

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0a2'
+version = '1.0a3'
 
 setup(name='plone.testing',
       version=version,
-      description="Testing infrastructure for Zope and Plone packages",
+      description="Testing infrastructure for Zope and Plone projects.",
       long_description=open("README.txt").read() + "\n\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n\n" +
+                       open("CHANGES.txt").read() + "\n\n" +
                        "Detailed documentation\n" +
                        "======================\n\n" +
                        open(os.path.join("src", "plone", "testing", "layer.txt")).read() + "\n\n" +
                        open(os.path.join("src", "plone", "testing", "zca.txt")).read() + "\n\n" +
                        open(os.path.join("src", "plone", "testing", "publisher.txt")).read() + "\n\n" +
                        open(os.path.join("src", "plone", "testing", "zodb.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         ],
       keywords='plone zope testing',
-      author='Martin Aspeli',
+      author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://pypi.python.org/pypi/plone.testing',
-      license='GPL',
+      license='GPL version 2',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['plone'],
@@ -68,6 +70,4 @@ setup(name='plone.testing',
                 'zope.publisher',
             ],
       },
-      entry_points="""
-      """,
       )
