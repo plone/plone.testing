4ef80de1735db6683859a8ce7c0fbbf739d42862
Godefroid Chapelle <gotcha@bubblenet.be>
2011-04-05 12:23:56 +0000

    Merge r47305 and r47494 from trunk
    zope.globalrequest support
    setup request.PARENTS
    
    svn path=/plone.testing/branches/3.x/; revision=48423

--- src/plone/testing/z2.py
+++ src/plone/testing/z2.py
@@ -677,18 +677,35 @@ class IntegrationTesting(Layer):
         
         app = addRequestContainer(Zope2.app(), environ=environ)
         
+        request = app.REQUEST
+        request['PARENTS'] = [app]
+        
+        # Make sure we have a zope.globalrequest request
+        try:
+            from zope.globalrequest import setRequest
+            setRequest(request)
+        except ImportError:
+            pass
+
         # Start a transaction
         transaction.begin()
         
         # Save resources for tests to access
         self['app'] = app
-        self['request'] = app.REQUEST
+        self['request'] = request
     
     def testTearDown(self):
         import transaction
         
         # Abort the transaction
         transaction.abort()
+        
+        # Make sure we have a zope.globalrequest request
+        try:
+            from zope.globalrequest import setRequest
+            setRequest(None)
+        except ImportError:
+            pass
         
         # Close the database connection and the request
         app = self['app']
@@ -748,18 +765,35 @@ class FunctionalTesting(Layer):
         
         app = addRequestContainer(Zope2.app(), environ=environ)
         
+        request = app.REQUEST
+        request['PARENTS'] = [app]
+        
+        # Make sure we have a zope.globalrequest request
+        try:
+            from zope.globalrequest import setRequest
+            setRequest(request)
+        except ImportError:
+            pass
+        
         # Start a transaction
         transaction.begin()
         
         # Save resources for the test
         self['app'] = app
-        self['request'] = app.REQUEST
+        self['request'] = request
     
     def testTearDown(self):
         import transaction
         
         # Abort any open transactions
         transaction.abort()
+        
+        # Make sure we have a zope.globalrequest request
+        try:
+            from zope.globalrequest import setRequest
+            setRequest(None)
+        except ImportError:
+            pass
         
         # Close the database connection and the request
         app = self['app']
