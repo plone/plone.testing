7272ab8f6ff1f8c367852d493e15ec4c14366450
Maurits van Rees <maurits@vanrees.org>
2015-07-29 23:47:31 +0200

    Back to development: 4.0.15
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os.path
 import sys
 from setuptools import setup, find_packages
 
-version = '4.0.14'
+version = '4.0.15.dev0'
 
 install_requires = ['setuptools',
                     'zope.testing',
