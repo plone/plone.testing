b999eafab0c3e424c57d29b986c8a5dca7e24ac0
Maurits van Rees <maurits@vanrees.org>
2018-02-05 10:43:01 +0100

    Claim only Plone 5.2 in the classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -47,9 +47,7 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.2",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
